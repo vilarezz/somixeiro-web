72d58435dd6732ba1b57575b8568c1f60c647d4d
Gil Vilares <vilarezz@its-a-trap.local>
2021-05-08 21:46:25 +0100

    Home image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,6 +63,7 @@ export default function Home() {
         <Image
           alt={'Login Image'}
           objectFit={'cover'}
+          height="100%"
           src={
             'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
           }
